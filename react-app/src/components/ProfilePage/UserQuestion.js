de051861096083d1539d54b108526a53639649f2
Hanna Rosenfeld <hannarosenfeld@Hannas-Air.lan>
2023-07-18 09:55:07 -0400

    Show number of answers in user question.

--- react-app/src/components/ProfilePage/UserQuestion.js
+++ react-app/src/components/ProfilePage/UserQuestion.js
@@ -3,10 +3,11 @@ import OpenModalButton from "../OpenModalButton";
 import AddAnswerModal from '../AddAnswerModal';
 
 function UserQuestion({ question }) {
-    console.log(question)
+    console.log("question",question)
     return (
         <div className="user-question">
             <h4><NavLink to={`/questions/${question.id}`}>{question.title}</NavLink></h4>
+            <div style={{fontSize: "13px", fontWeight: "bold", marginTop: "10px", color: "var(--qgrey"}}>{question.answers.length} answers</div>
             <div style={{display: "flex", gap: "0.3em", alignItems: "baseline"}}>
                 <span><i class="fa-regular fa-pen-to-square"></i></span>
                 <OpenModalButton 
