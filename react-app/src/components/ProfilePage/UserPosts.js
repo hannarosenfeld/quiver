2e40df458f880afc58e426976c53362542062a66
Hanna Rosenfeld <hannarosenfeld@Hannas-Air.lan>
2023-07-19 08:31:56 -0400

    Styling.

--- react-app/src/components/ProfilePage/UserPosts.js
+++ react-app/src/components/ProfilePage/UserPosts.js
@@ -27,21 +27,17 @@ function UserPosts({ post, user }) {
             
             <div className="edit-question-container" style={{display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: "1em"}}>
                 {/* UP AND DOWN VOTE */}
-                <div className="updown-vote">
-                    <div className="upvote"><i class="fa-solid fa-arrow-up"></i><span>Upvote</span></div>
-                    <div><i class="fa-solid fa-arrow-down"></i></div>
+                <div style={{display: "flex", gap: "20px", alignItems: "center"}}>
+                    <div className="updown-vote">
+                        <div className="upvote"><i class="fa-solid fa-arrow-up"></i><span>Upvote</span></div>
+                        <div><i class="fa-solid fa-arrow-down"></i></div>
                 </div>
                 <i onClick={() => setToggle(!toggle)}className="fa-regular fa-comment" style={{cursor: "pointer", marginTop: "0.5em"}}></i>
-                <OpenModalButton
+                </div> <OpenModalButton
                     buttonText="Delete"
                     modalComponent={<DeletePostModal post={post}/>}
                 />     
             </div>
-
-            <div className="updown-vote" style={{marginTop: "10px", height: "30px"}}>
-                <div className="upvote"><i class="fa-solid fa-arrow-up"></i><span style={{marginLeft: "5px"}}>Upvote</span></div>
-            <div><i class="fa-solid fa-arrow-down"></i></div>
-           </div>
         </div>
     )
 }
